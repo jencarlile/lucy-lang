3ea5a89f2fa5365260742f8821d60a5982a62361
Jen Carlile <jcarlile@gmail.com>
2018-03-21 15:28:26 -0700

    Add ability to parse functions

--- src/lucy/ast/ast.go
+++ src/lucy/ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"lucy/token"
+	"strings"
 )
 
 // Every node in the AST must implement the Node interface
@@ -218,6 +219,7 @@ func (ie *IfExpression) String() string {
 	return out.String()
 }
 
+//-----------------------------------------------------------------------------
 type BlockStatement struct {
 	Token      token.Token // the { token
 	Statements []Statement
@@ -230,6 +232,31 @@ func (bs *BlockStatement) String() string {
 	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
+
+	return out.String()
+}
+
+//-----------------------------------------------------------------------------
+type FunctionLiteral struct {
+	Token      token.Token // The 'fn' token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
 
 	return out.String()
 }
